7cf087c7984543b2632878ca6f4cf1a5f3fae78f
Etienne G <Moujimouja@users.noreply.github.com>
2020-04-08 09:37:49 +0200

    Adjust contribute link and "read the doc" label (#47)
    * adjust contribute link and read the doc label
    
    * Add the contribute label on FR page
    
    Co-authored-by: Jérémy Delpierre <jdelpierre@users.noreply.github.com>

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -24,6 +24,7 @@ const links = {
   slack: 'https://centreon.github.io/register-slack/',
   twitter: 'https://twitter.com/CentreonFR/',
   linkedin: 'https://www.linkedin.com/company/merethis/',
+  contribute: 'https://github.com/centreon/centreon/blob/master/CONTRIBUTING.md',
 }
 
 const stringsAndParagraphs = {
@@ -195,7 +196,7 @@ function HeartOpenSourceBlock() {
         <p className="textOpenSource">{stringsAndParagraphs.heartOpenSourceBlock.content}</p>
       </div>
       <div className="cardBarOpenSource">
-        <Card imageSrc={'icon-openSource.svg'} btnLabel={stringsAndParagraphs.heartOpenSourceBlock.btnOpen} btnLink={links.github} />
+        <Card imageSrc={'icon-openSource.svg'} btnLabel={stringsAndParagraphs.heartOpenSourceBlock.btnOpen} btnLink={links.contribute} />
       </div>
     </div>
   )
