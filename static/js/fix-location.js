5a4dbcb21edd1f4e648dbe49e2c68c8b73ebdf7a
Shonan10 <smekhalfia@centreon.com>
2022-02-10 09:03:45 +0100

    Tests CI/CD

--- static/js/fix-location.js
+++ static/js/fix-location.js
@@ -2,3 +2,7 @@ if (window && window.location && window.location.pathname.endsWith('/') && windo
 {
    window.history.replaceState('', '', window.location.pathname.substr(0, window.location.pathname.length - 1))
 }
+if (window && window.location && window.location.pathname.includes('/#') && window.location.pathname !== '/#')
+{
+   window.history.replaceState('', '', window.location.pathname.substr(0, window.location.pathname.length - 1))
+}
