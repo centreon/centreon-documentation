cc42f7eb6229d75a5d94fa01a6ae4a802ea0bb40
cgenier <cgenier@centreon.com>
2022-09-26 14:29:27 +0200

    Add DocCard theme override + test topic in ToC

---
+++ src/theme/DocCard/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import DocCard from '@theme-original/DocCard';
+
+export default function DocCardWrapper(props) {
+  return (
+    <>
+      <DocCard {...props} />
+    </>
+  );
+}
