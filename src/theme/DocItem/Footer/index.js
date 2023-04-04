82c9cc4bae906232737abaa912cb921d5bdf73a3
Thomas <thomaspopoff@gmail.com>
2023-04-04 14:13:16 +0200

    same row for footer doc page elements

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -30,44 +30,40 @@ function EditMetaRow({
 }) {
   const location = useLocation();
   return (
-    <div className={clsx(ThemeClassNames.docs.docFooterEditMetaRow)}>
-      <div className="row">
-        <div className="col">{editUrl && <EditThisPage editUrl={editUrl} /> }</div>
-        <div className="col">
+    <div className={clsx(ThemeClassNames.docs.docFooterEditMetaRow, 'row')}>
+      <div className="col">{editUrl && <EditThisPage editUrl={editUrl} /> }</div>
+      <div className="col">
 
-          <svg id="feedback" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-              viewBox="0 0 24 24"  xml:space="preserve" fill="none" style={{ height: '24px', width: '24px', verticalAlign:"text-top", marginRight: '4px' }} >
-            <rect y="0" class="st0" width="24" height="24"/>
-            <rect x="2" y="2" class="st0" width="20" height="20"/>
-            <path fill="none" stroke="currentColor" stroke-miterlimit="10" d="M16.14,9.08c0,3.09-2.76,5.6-6.16,5.6c-1.11,0-5.45,0-5.45,0s1.36-1.38,0.93-1.79c-1.02-1-1.64-2.34-1.64-3.81
-              c0-3.09,2.76-5.6,6.16-5.6S16.14,5.99,16.14,9.08z"/>
-            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" d="M8.61,16.28c0.76,2.31,3.12,4,5.91,4c1.11,0,5.45,0,5.45,0s-1.36-1.38-0.93-1.79c1.02-1,1.64-2.34,1.64-3.81
-              c0-2-1.15-3.75-2.88-4.74"/>
-            <line fill="none" stroke="currentColor" stroke-linecap="round" x1="6.78" y1="6.86" x2="13.55" y2="6.86"/>
-            <line fill="none" stroke="currentColor" stroke-linecap="round" x1="6.78" y1="8.74" x2="13.55" y2="8.74"/>
-            <line fill="none" stroke="currentColor" stroke-linecap="round" x1="6.78" y1="10.62" x2="13.55" y2="10.62"/>
-          </svg>
+        <svg id="feedback" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 24 24"  xml:space="preserve" fill="none" style={{ height: '24px', width: '24px', verticalAlign:"text-top", marginRight: '4px' }} >
+          <rect y="0" class="st0" width="24" height="24"/>
+          <rect x="2" y="2" class="st0" width="20" height="20"/>
+          <path fill="none" stroke="currentColor" stroke-miterlimit="10" d="M16.14,9.08c0,3.09-2.76,5.6-6.16,5.6c-1.11,0-5.45,0-5.45,0s1.36-1.38,0.93-1.79c-1.02-1-1.64-2.34-1.64-3.81
+            c0-3.09,2.76-5.6,6.16-5.6S16.14,5.99,16.14,9.08z"/>
+          <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" d="M8.61,16.28c0.76,2.31,3.12,4,5.91,4c1.11,0,5.45,0,5.45,0s-1.36-1.38-0.93-1.79c1.02-1,1.64-2.34,1.64-3.81
+            c0-2-1.15-3.75-2.88-4.74"/>
+          <line fill="none" stroke="currentColor" stroke-linecap="round" x1="6.78" y1="6.86" x2="13.55" y2="6.86"/>
+          <line fill="none" stroke="currentColor" stroke-linecap="round" x1="6.78" y1="8.74" x2="13.55" y2="8.74"/>
+          <line fill="none" stroke="currentColor" stroke-linecap="round" x1="6.78" y1="10.62" x2="13.55" y2="10.62"/>
+        </svg>
 
-          <a rel="noreferrer noopener" href={`https://docs.google.com/forms/d/e/1FAIpQLSdeKkEHWbxn4ZweR1M4OEMzP_QoqKmjl6t4iC_yKeX2CFjzAw/viewform?usp=pp_url&entry.371845164=docs.centreon.com${location.pathname}`} target="_blank">
-          <Translate
-            id="theme.common.feedback"
-            description="The link label to give feedback on the page">
-            Give feedback on this page
-          </Translate>
+        <a rel="noreferrer noopener" href={`https://docs.google.com/forms/d/e/1FAIpQLSdeKkEHWbxn4ZweR1M4OEMzP_QoqKmjl6t4iC_yKeX2CFjzAw/viewform?usp=pp_url&entry.371845164=docs.centreon.com${location.pathname}`} target="_blank">
+        <Translate
+          id="theme.common.feedback"
+          description="The link label to give feedback on the page">
+          Give feedback on this page
+        </Translate>
 
-          </a>
-        </div>
+        </a>
       </div>
-      <div className="row">
-        <div className={clsx('col', styles.lastUpdated)}>
-          {(lastUpdatedAt || lastUpdatedBy) && (
-            <LastUpdated
-              lastUpdatedAt={lastUpdatedAt}
-              formattedLastUpdatedAt={formattedLastUpdatedAt}
-              lastUpdatedBy={lastUpdatedBy}
-            />
-          )}
-        </div>
+      <div className={clsx('col', styles.lastUpdated)}>
+        {(lastUpdatedAt || lastUpdatedBy) && (
+          <LastUpdated
+            lastUpdatedAt={lastUpdatedAt}
+            formattedLastUpdatedAt={formattedLastUpdatedAt}
+            lastUpdatedBy={lastUpdatedBy}
+          />
+        )}
       </div>
     </div>
   );
