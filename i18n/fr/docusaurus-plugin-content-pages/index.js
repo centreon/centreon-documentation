efb0af7a17619e6ee0257fe7f037b4937341fb50
smau <102975713+smau-centreon@users.noreply.github.com>
2023-05-15 16:37:33 +0200

    Report PR #2264 to 23.10 (#2314)

--- i18n/fr/docusaurus-plugin-content-pages/index.js
+++ i18n/fr/docusaurus-plugin-content-pages/index.js
@@ -20,7 +20,7 @@ const links = {
   twitter: 'https://twitter.com/CentreonFR/',
   linkedin: 'https://www.linkedin.com/company/merethis/',
   contribute:
-    'https://github.com/centreon/centreon/blob/master/CONTRIBUTING.md',
+    'https://github.com/centreon/.github/blob/master/CONTRIBUTING.md',
   banner:
     'https://thewatch.centreon.com/',
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ const links = {
   slack: 'https://centreon.github.io/register-slack/',
   twitter: 'https://twitter.com/CentreonFR/',
   linkedin: 'https://www.linkedin.com/company/merethis/',
-  contribute:'https://github.com/centreon/centreon/blob/master/CONTRIBUTING.md',
+  contribute:'https://github.com/centreon/.github/blob/master/CONTRIBUTING.md',
   banner: 'https://thewatch.centreon.com/',
 };
 
