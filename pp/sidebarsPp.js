4aad7280419ed1cff17f95a7b78a7f0e1014ba88
qgarnier <garnier.quentin@gmail.com>
2023-08-11 10:39:25 +0200

    add(epp): applications-tosca-restapi (#2428)
    * add(epp): applications-tosca-restapi
    
    * Update pp/integrations/plugin-packs/procedures/applications-tosca-restapi.md
    
    Co-authored-by: cg-tw <83637804+cg-tw@users.noreply.github.com>
    
    * Update i18n/fr/docusaurus-plugin-content-docs-pp/current/integrations/plugin-packs/procedures/applications-tosca-restapi.md
    
    * Update pp/integrations/plugin-packs/procedures/applications-tosca-restapi.md
    
    ---------
    
    Co-authored-by: cg-tw <83637804+cg-tw@users.noreply.github.com>
    Co-authored-by: omercier <32134301+omercier@users.noreply.github.com>

--- pp/sidebarsPp.js
+++ pp/sidebarsPp.js
@@ -540,6 +540,10 @@ module.exports = {
         },
         {
           type: 'doc',
+          id: 'integrations/plugin-packs/procedures/applications-tosca-restapi'
+        },
+        {
+          type: 'doc',
           id: 'integrations/plugin-packs/procedures/applications-trendmicro-iwsva-snmp'
         },
         {
