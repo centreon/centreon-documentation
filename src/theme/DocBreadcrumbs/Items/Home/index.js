989f18ecea80a3f74f07f5c5795731f3cc4ef27d
Kevin Duret <kduret@centreon.com>
2024-02-20 16:17:30 +0100

    fix homepage links

--- src/theme/DocBreadcrumbs/Items/Home/index.js
+++ src/theme/DocBreadcrumbs/Items/Home/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import Link from '@docusaurus/Link';
-import { useActiveDocContext } from '@docusaurus/plugin-content-docs/client';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import {translate} from '@docusaurus/Translate';
+import {useLocation} from '@docusaurus/router';
 import IconHome from '@theme/Icon/Home';
 import styles from './styles.module.css';
 
 export default function HomeBreadcrumbItem(props) {
-  const { activeDoc } = useActiveDocContext(props.docsPluginId);
+  const location = useLocation();
+  const docSection = location.pathname.split("/")[1];
 
   // Hack: Swizzle component to use baseUrl until https://github.com/facebook/docusaurus/issues/6953
-  const homeHref = useBaseUrl(
-    activeDoc
-      ? "/docs/getting-started/welcome"
-      : "/pp/integrations/plugin-packs/getting-started/introduction"
-  );
+  let homePath = '/docs/getting-started/welcome';
+  if (docSection === 'pp') {
+    homePath = '/pp/integrations/plugin-packs/getting-started/introduction';
+  } else if (docSection === 'cloud') {
+    homePath = '/cloud/getting-started/welcome';
+  }
+
+  const homeHref = useBaseUrl(homePath);
 
   return (
     <li className="breadcrumbs__item">
