cf58fa39d8c22f9791786749f0387f800a1a6a81
YanMeddour <yan.meddour2001@gmail.com>
2024-09-17 16:03:38 +0200

    Improved help request buttons

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -5,14 +5,12 @@ import { useLocation } from '@docusaurus/router';
 import { useDoc } from '@docusaurus/theme-common/internal';
 import styles from './styles.module.css';
 import Translate from '@docusaurus/Translate';
-import useBaseUrl from '@docusaurus/useBaseUrl';
+
 
 export default function TOC(props) {
   const [showFooterButtons, setShowFooterButtons] = useState(false);
   const location = useLocation();
   const { metadata } = useDoc();
-  const imagePath = useBaseUrl('/img/image.png');
-
 
   const editUrl = metadata?.editUrl || '';
 
@@ -151,12 +149,93 @@ export default function TOC(props) {
             </a>
           </div>
         )}
-        <img
-          src={imagePath}
-          alt="help button"
+        <div
           className={styles.pngImage}
           onClick={handleToggleButtons}
-        />
+          aria-label="Toggle buttons"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="50" height="50" viewBox="0 0 80 80">
+            <defs>
+              <filter id="a" x="13" y="14" width="54" height="54" filterUnits="userSpaceOnUse">
+                <feOffset dy="2" input="SourceAlpha"/>
+                <feGaussianBlur stdDeviation="1" result="b"/>
+                <feFlood flood-opacity="0.2"/>
+                <feComposite operator="in" in2="b"/>
+                <feComposite in="SourceGraphic"/>
+              </filter>
+              <clipPath id="c">
+                <rect width="32" height="34" fill="#fff"/>
+              </clipPath>
+              <filter id="d" x="13" y="14" width="54" height="54" filterUnits="userSpaceOnUse">
+                <feOffset dy="2" input="SourceAlpha"/>
+                <feGaussianBlur stdDeviation="1" result="e"/>
+                <feFlood flood-opacity="0.2"/>
+                <feComposite operator="in" in2="e"/>
+                <feComposite in="SourceGraphic"/>
+              </filter>
+              <clipPath id="f">
+                <rect width="36" height="36" fill="#fff"/>
+              </clipPath>
+              <filter id="g" x="13" y="14" width="54" height="54" filterUnits="userSpaceOnUse">
+                <feOffset dy="2" input="SourceAlpha"/>
+                <feGaussianBlur stdDeviation="1" result="h"/>
+                <feFlood flood-opacity="0.2"/>
+                <feComposite operator="in" in2="h"/>
+                <feComposite in="SourceGraphic"/>
+              </filter>
+              <clipPath id="i">
+                <rect width="30.758" height="31.674" transform="translate(0 0)" fill="#fff"/>
+              </clipPath>
+            </defs>
+            <g transform="translate(-1678 -980)">
+              <g transform="translate(1694 995)">
+                <g transform="matrix(1, 0, 0, 1, -16, -15)" filter="url(#a)">
+                  <rect width="48" height="48" rx="24" transform="translate(16 15)" fill="#37d6ae"/>
+                </g>
+                <g transform="translate(6 6)">
+                  <rect width="36" height="36" fill="none"/>
+                  <g transform="translate(2 1)">
+                    <g clipPath="url(#c)">
+                      <path d="M0,27.818H32V34H0Zm6.4-6.182H8.64L21.12,9.62,19.96,8.5,18.84,7.418,6.4,19.473ZM3.2,24.727V18.159L21.12.889A3.129,3.129,0,0,1,22.139.232,3.261,3.261,0,0,1,23.36,0,3.41,3.41,0,0,1,24.6.232a3.207,3.207,0,0,1,1.08.7l2.2,2.164a2.526,2.526,0,0,1,.7,1,3.368,3.368,0,0,1,.219,1.2,3.107,3.107,0,0,1-.219,1.139,2.9,2.9,0,0,1-.7,1.025L10,24.727ZM25.6,5.255,23.36,3.091ZM21.12,9.62,19.96,8.5,18.84,7.418Z" fill="#fff"/>
+                    </g>
+                  </g>
+                </g>
+              </g>
+              <g transform="translate(1694 995)">
+                <g transform="matrix(1, 0, 0, 1, -16, -15)" filter="url(#d)">
+                  <rect width="48" height="48" rx="24" transform="translate(16 15)" fill="#37d6ae"/>
+                </g>
+                <g transform="translate(6 6)">
+                  <g clipPath="url(#f)">
+                    <path d="M13.382,23.127H1.362L3,21.466a7.817,7.817,0,0,0,1.484-1.989A10.391,10.391,0,0,1,1.447,12.2C1.447,6.171,6.8,1.269,13.382,1.269S25.314,6.171,25.314,12.2,19.959,23.127,13.382,23.127M5.757,21.175h7.625c5.5,0,9.981-4.028,9.981-8.979s-4.479-8.979-9.981-8.979S3.4,7.245,3.4,12.2a8.482,8.482,0,0,0,2.626,6.069,1.471,1.471,0,0,1,.441,1.068,3.569,3.569,0,0,1-.708,1.842" transform="translate(0.681 0.635)" fill="#fff"/>
+                    <path d="M30.614,29.255H18.59c-5.229,0-9.93-3.2-11.43-7.784a.975.975,0,1,1,1.852-.608A9.96,9.96,0,0,0,18.59,27.3h7.625a3.583,3.583,0,0,1-.708-1.84,1.478,1.478,0,0,1,.441-1.069,8.475,8.475,0,0,0,2.625-6.068,8.774,8.774,0,0,0-4.634-7.582.975.975,0,0,1,.969-1.692,10.7,10.7,0,0,1,5.614,9.274,10.38,10.38,0,0,1-3.03,7.278,7.838,7.838,0,0,0,1.483,1.991Z" transform="translate(3.556 4.461)" fill="#fff"/>
+                    <path d="M17.961,7.214H5.916a.975.975,0,0,1,0-1.95H17.961a.975.975,0,1,1,0,1.95" transform="translate(2.47 2.632)" fill="#fff"/>
+                    <path d="M17.961,9.444H5.916a.975.975,0,1,1,0-1.95H17.961a.975.975,0,1,1,0,1.95" transform="translate(2.47 3.747)" fill="#fff"/>
+                    <path d="M17.961,11.675H5.916a.975.975,0,1,1,0-1.95H17.961a.975.975,0,1,1,0,1.95" transform="translate(2.47 4.863)" fill="#fff"/>
+                  </g>
+                </g>
+              </g>
+              <g transform="translate(1694 995)">
+                <g transform="matrix(1, 0, 0, 1, -16, -15)" filter="url(#g)">
+                  <rect width="48" height="48" rx="24" transform="translate(16 15)" fill="#37d6ae"/>
+                </g>
+                <g transform="translate(6 6)">
+                  <rect width="36" height="36" fill="none"/>
+                  <g transform="translate(2.436 2.163)">
+                    <g transform="translate(0 0)" clipPath="url(#i)">
+                      <path d="M14.921,31.674a15.71,15.71,0,0,1-9.47-3.141,1.7,1.7,0,0,1-.645-1.71,1.668,1.668,0,0,1,1.054-1.229A9.8,9.8,0,0,0,9.233,21.51c2.788-6.454,7.166-6.825,7.778-6.845a8.986,8.986,0,0,1,5.181,1.078,3.1,3.1,0,0,0-.014-2.327,3.7,3.7,0,0,0-2.617-1.5l-.641-.076-.323-.564a5.561,5.561,0,0,0-3.611-2.849C10.3,7.247,3.7,11.206,2.43,12.012A1.574,1.574,0,0,1,.018,10.785L0,10.51l.1-.258a15.845,15.845,0,0,1,30.656,5.2A15.837,15.837,0,0,1,14.921,31.674ZM8.016,27.2A13.284,13.284,0,0,0,24.29,6.432,13.266,13.266,0,0,0,4.031,8.2c.831-.414,1.776-.842,2.783-1.223,3.4-1.288,6.361-1.63,8.793-1.018A7.99,7.99,0,0,1,20.5,9.51a5.9,5.9,0,0,1,3.917,2.7c.885,1.652.689,3.726-.581,6.163l-1,1.917L21.68,18.537c-.126-.151-1.359-1.531-4.537-1.333l-.065,0h-.016c-.267.011-3.319.267-5.5,5.305A13.038,13.038,0,0,1,8.014,27.2Zm-1.649-.437h0Z" transform="translate(0 0.001)" fill="#fff"/>
+                      <path d="M157.749,131.8a3.848,3.848,0,0,1-2.55.367,1.8,1.8,0,0,0-.969.035,1,1,0,0,1-.021-.7c.646-1.552,3.053-1.216,3.54.3" transform="translate(-141.949 -120.15)" fill="#fff"/>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+            <circle cx="40" cy="40" r="40" fill="#36d6ae"/>
+            <text transform="translate(26 60)" fill="#fff" fontSize="57" fontFamily="Roboto-Medium, Roboto" fontWeight="500">
+              <tspan x="0" y="0">?</tspan>
+            </text>
+          </svg>
+        </div>
       </div>
     </div>
   );
