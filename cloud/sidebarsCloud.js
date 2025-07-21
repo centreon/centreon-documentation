683d6db33b9ef9d19b92e6527d74890bf3f5bdc0
smau <smau@centreon.com>
2025-07-21 12:18:31 +0200

    Document Meta services - Cloud

--- cloud/sidebarsCloud.js
+++ cloud/sidebarsCloud.js
@@ -276,6 +276,10 @@ module.exports = {
             {
               "type": "doc",
               "id": "monitoring/basic-objects/services-templates"
+            },
+            {
+              "type": "doc",
+              "id": "monitoring/basic-objects/meta-services"
             }
           ]
         },
