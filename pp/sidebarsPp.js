fa7b052bb50b2a762b132cae36552ffe8eef9405
lucie-tirand <ldubrunfaut@CNTR-PORT-A198.localdomain>
2025-11-10 18:05:44 +0100

    doc(network-switchs-alcatel-omniswitch-6850): remove doc page CTOR-2017

--- pp/sidebarsPp.js
+++ pp/sidebarsPp.js
@@ -2005,10 +2005,6 @@ module.exports = {
         },
         {
           type: 'doc',
-          id: 'integrations/plugin-packs/procedures/network-switchs-alcatel-omniswitch-6850'
-        },
-        {
-          type: 'doc',
           id: 'integrations/plugin-packs/procedures/network-oneaccess-snmp'
         },
         {
