61ed6c67f85bbe8fc55d6b7b775c742b076a25c8
lucie-tirand <ldubrunfaut@CNTR-PORT-A198.localdomain>
2025-12-16 16:17:04 +0100

    feat(applications-monitoring-centreon-dem-restapi) and deprecate(applications-monitoring-quanta-restapi) CTOR-2076

--- pp/sidebarsPp.js
+++ pp/sidebarsPp.js
@@ -787,6 +787,10 @@ module.exports = {
         },
         {
           type: 'doc',
+          id: 'integrations/plugin-packs/procedures/applications-monitoring-centreon-dem-restapi'
+        },
+        {
+          type: 'doc',
           id: 'integrations/plugin-packs/procedures/applications-monitoring-centreon-map-jmx'
         },
         {
@@ -1575,14 +1579,6 @@ module.exports = {
         },
         {
           type: 'doc',
-          id: 'integrations/plugin-packs/procedures/network-security-cato-networks-api'
-        },
-        {
-          type: 'doc',
-          id: 'integrations/plugin-packs/procedures/network-hpe-athonet-alertmanager-api'
-        },
-        {
-          type: 'doc',
           id: 'integrations/plugin-packs/procedures/network-athonet-epc-snmp'
         },
         {
@@ -1628,6 +1624,10 @@ module.exports = {
         {
           type: 'doc',
           id: 'integrations/plugin-packs/procedures/network-cambium-epmp-snmp'
+        },
+        {
+          type: 'doc',
+          id: 'integrations/plugin-packs/procedures/network-security-cato-networks-api'
         },
         {
           type: 'doc',
