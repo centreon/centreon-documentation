d9d143f8ab0ac434fb9e4541e3428894397c1c74
Kevin Duret <kduret@centreon.com>
2026-01-19 10:10:19 +0100

    fix(ci): use runner ubuntu-24.04

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,11 +3,6 @@ import { themes as prismThemes } from 'prism-react-renderer';
 import availableVersions from './versions.json';
 import archivedVersions from './archivedVersions.json';
 
-const isDev = process.env.BRANCH_NAME !== 'staging';
-if (isDev) {
-  console.log('Building documentation in development mode, last update time of each page will not be shown.');
-}
-
 const archivedVersion = process.env.ARCHIVED_VERSION ?? null;
 
 const versions = (() => {
@@ -112,7 +107,7 @@ const config = {
           admonitions: {},
           editUrl: 'https://github.com/centreon/centreon-documentation/edit/staging/',
           editLocalizedFiles: true,
-          showLastUpdateTime: !isDev,
+          showLastUpdateTime: true,
           includeCurrentVersion: false,
           onlyIncludeVersions: versions,
           versions: (() => {
@@ -220,7 +215,7 @@ const config = {
             breadcrumbs: true,
             editUrl: 'https://github.com/centreon/centreon-documentation/edit/staging/',
             editLocalizedFiles: true,
-            showLastUpdateTime: !isDev,
+            showLastUpdateTime: true,
           },
         ],
       ];
@@ -239,7 +234,7 @@ const config = {
             breadcrumbs: true,
             editUrl: 'https://github.com/centreon/centreon-documentation/edit/staging/',
             editLocalizedFiles: true,
-            showLastUpdateTime: !isDev,
+            showLastUpdateTime: true,
           },
         ],
       ];
@@ -258,7 +253,7 @@ const config = {
             breadcrumbs: true,
             editUrl: 'https://github.com/centreon/centreon-documentation/edit/staging/',
             editLocalizedFiles: true,
-            showLastUpdateTime: !isDev,
+            showLastUpdateTime: true,
           },
         ],
       ];
