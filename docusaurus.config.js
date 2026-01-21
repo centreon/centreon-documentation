54e804afe526fd2a8993cbf7325b5235c8c7c0e3
Kevin Duret <kduret@centreon.com>
2026-01-21 17:25:13 +0100

    fix(css): update background color of product dropdown (#4991)
    * fix(css): update background color of product dropdown
    
    * fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,8 +62,20 @@ const config = {
   },
 
   future: {
-    v4: true,
-    experimental_faster: true,
+    v4: {
+      removeLegacyPostBuildHeadAttribute: false,
+      useCssCascadeLayers: true,
+    },
+    experimental_faster: {
+      swcJsLoader: false,
+      swcJsMinimizer: false,
+      swcHtmlMinimizer: false,
+      lightningCssMinimizer: false,
+      rspackBundler: true,
+      rspackPersistentCache: true,
+      ssgWorkerThreads: false,
+      mdxCrossCompilerCache: false,
+    },
   },
 
   title: 'Centreon Documentation',
