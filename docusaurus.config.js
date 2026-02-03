caadce30c41781506f9874b42738375b1785f341
Kevin Duret <kduret@centreon.com>
2026-02-03 16:49:19 +0100

    fix(ci): improve build memory usage

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -210,18 +210,18 @@ const config = {
 
   plugins: (() => {
     let plugins = [
-      [
-        '@docusaurus/plugin-ideal-image',
-        {
-          quality: 70,
-          max: 1030, // max resized image's size.
-          min: 640, // min resized image's size. if original is lower, use that size.
-          steps: 2, // the max number of images generated between min and max (inclusive)
-          // Use false to debug, but it incurs huge perf costs
-          disableInDev: true,
-        },
-      ],
-      'docusaurus-plugin-image-zoom',
+      // [
+      //   '@docusaurus/plugin-ideal-image',
+      //   {
+      //     quality: 70,
+      //     max: 1030, // max resized image's size.
+      //     min: 640, // min resized image's size. if original is lower, use that size.
+      //     steps: 2, // the max number of images generated between min and max (inclusive)
+      //     // Use false to debug, but it incurs huge perf costs
+      //     disableInDev: true,
+      //   },
+      // ],
+      // 'docusaurus-plugin-image-zoom',
     ];
 
     if (cloud) {
