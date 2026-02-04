d1dd84ff0ed23ef0357b49acb4e054f544ce428d
Kevin Duret <kduret@centreon.com>
2026-02-04 10:40:18 +0100

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,17 +67,17 @@ if (versions.length == 0 && !pp && !cloud && !dem && !logmanagement) {
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 const config = {
-  // rspack: {
-  //   devtool: false,
-  //   optimization: {
-  //     splitChunks: {
-  //       chunks: 'async',
-  //     },
-  //   },
-  //   infrastructureLogging: {
-  //     level: 'error',
-  //   },
-  // },
+  rspack: {
+    devtool: false,
+    optimization: {
+      splitChunks: {
+        chunks: 'async',
+      },
+    },
+    infrastructureLogging: {
+      level: 'error',
+    },
+  },
 
   customFields: {
     version: archivedVersion ?? null,
