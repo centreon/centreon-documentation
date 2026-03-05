f7d9e09697fd9e56049c7d0b4c662254ec0a1300
Sylvain Cresto <scresto@centreon.com>
2026-03-05 08:58:51 +0100

    Initial doc

--- pp/sidebarsPp.js
+++ pp/sidebarsPp.js
@@ -194,6 +194,10 @@ module.exports = {
         },
         {
           type: 'doc',
+          id: 'integrations/plugin-packs/procedures/applications-dynamics-365-nsclient-05-nrpe'
+        },
+        {
+          type: 'doc',
           id: 'integrations/plugin-packs/procedures/applications-monitoring-dynatrace-restapi'
         },
         {
@@ -375,10 +379,6 @@ module.exports = {
         {
           type: 'doc',
           id: 'integrations/plugin-packs/procedures/applications-dynamics-ax-nsclient-05-nrpe'
-        },
-        {
-          type: 'doc',
-          id: 'integrations/plugin-packs/procedures/applications-dynamics-365-nsclient-05-nrpe'
         },
         {
           type: 'doc',
@@ -1947,6 +1947,10 @@ module.exports = {
         {
           type: 'doc',
           id: 'integrations/plugin-packs/procedures/network-juniper-trapeze-snmp'
+        },
+        {
+          type: 'doc',
+          id: 'integrations/plugin-packs/procedures/network-kairos-snmp'
         },
         {
           type: 'doc',
