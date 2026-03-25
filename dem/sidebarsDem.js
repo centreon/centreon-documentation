32504ff06b5cdcb6cc8a6469b058da2d02818d2c
smau <smau@centreon.com>
2026-03-25 15:29:20 +0100

    Organize CXM ToC

--- dem/sidebarsDem.js
+++ dem/sidebarsDem.js
@@ -2,132 +2,63 @@ module.exports = {
   dem: [
     {
       type: 'category',
-      label: 'Getting started with Centreon DEM',
+      label: 'Discovering CXM',
       link: {
         type: "doc",
         id: "getting-started/welcome"
       },
       items: [
        { type: 'doc', id: 'getting-started/dem-solution' },
-        { type: 'doc', id: 'getting-started/contact-support' },
         { type: 'doc', id: 'getting-started/synthetic-monitoring' },
         { type: 'doc', id: 'getting-started/real-user-monitoring' },
         { type: 'doc', id: 'getting-started/system-view' },
         { type: 'doc', id: 'getting-started/business-view' },
         { type: 'doc', id: 'getting-started/load-tests' },
-        { type: 'doc', id: 'getting-started/dashboards' },
-      ],
+       ],
     },
     {
       type: 'category',
-      label: 'Installation',
+      label: 'Setting up CXM',
       link: {
         type: 'generated-index',
       },
       items: [
         { type: 'doc', id: 'installation/installation-checklist' },
-        { type: 'doc', id: 'installation/dem-ip-addresses' },
-        { type: 'doc', id: 'installation/real-user-monitoring-installation' },
-        { type: 'doc', id: 'installation/monitor-production-events' },
-        {
-          collapsed: true,
-          type: 'category',
-          label: 'Servers and middleware',
-          link: {
-            type: 'generated-index',
-          },
-          items: [
-            { type: 'doc', id: 'installation/servers/install-system-agents' },
-            { type: 'doc', id: 'installation/servers/cloud-configuration-of-agents' },
-            { type: 'doc', id: 'installation/servers/add-advanced-metrics' },
-            { type: 'doc', id: 'installation/servers/install-php-magento-orocommerce-profiler' },
-          ],
-        },
+        
       ],
     },
     {
       type: 'category',
-      label: 'Configuration',
+      label: 'Getting started with CXM',
       link: {
         type: 'generated-index',
       },
       items: [
         { type: 'doc', id: 'configuration/configuration-checklist' },
-        { type: 'doc', id: 'configuration/manage-users-and-rights' },
-        { type: 'doc', id: 'configuration/configure-digital-sobriety' },
-        { type: 'doc', id: 'configuration/configure-google-analytics' },
-        { type: 'doc', id: 'configuration/receive-and-configure-alerts' },
-        {
-          collapsed: true,
-          type: 'category',
-          label: 'User journey',
-          link: {
-            type: 'generated-index',
-          },
-          items: [
-            { type: 'doc', id: 'configuration/user-journey/create-a-scenario' },
-            { type: 'doc', id: 'configuration/user-journey/user-journey-best-practices' },
-          ],
-        },
-        {
-          collapsed: true,
-          type: 'category',
-          label: 'Advanced configuration',
-          link: {
-            type: 'generated-index',
-          },
-          items: [
-            {
-              type: 'doc',
-              id: 'configuration/advanced-configuration/enable-disable-scenario-or-alert-via-api',
-            },
-          ],
-        },
+       
       ],
     },
     {
       type: 'category',
-      label: 'Performance analysis',
+      label: 'Synthetic Monitoring (STM)',
       link: {
         type: 'generated-index',
       },
       items: [
         { type: 'doc', id: 'performance-analysis/network-tab-indicators' },
-        { type: 'doc', id: 'performance-analysis/system-tab-indicators' },
-        { type: 'doc', id: 'performance-analysis/speed-up-website-with-applications-or-server-configuration' },
-        { type: 'doc', id: 'performance-analysis/errors-and-unavailability-front-end' },
-        {
-          collapsed: true,
-          type: 'category',
-          label: 'Metric overview',
-          link: {
-            type: 'doc',
-            id: 'performance-analysis/metrics/overview-of-metrics',
-          },
-          items: [
-            { type: 'doc', id: 'performance-analysis/metrics/hero-time' },
-            { type: 'doc', id: 'performance-analysis/metrics/time-to-first-byte' },
-            { type: 'doc', id: 'performance-analysis/metrics/speed-index' },
-            { type: 'doc', id: 'performance-analysis/metrics/on-load' },
-            { type: 'doc', id: 'performance-analysis/metrics/largest-contentful-paint' },
-            { type: 'doc', id: 'performance-analysis/metrics/total-blocking-time' },
-            { type: 'doc', id: 'performance-analysis/metrics/cumulative-layout-shift' },
-          ],
-        },
-        {
-          collapsed: true,
-          type: 'category',
-          label: 'Basic actions',
-          link: {
-            type: 'generated-index',
-          },
-          items: [
-            { type: 'doc', id: 'performance-analysis/basic-actions/navigate-in-dem' },
-            { type: 'doc', id: 'performance-analysis/basic-actions/how-alerts-work' },
-            { type: 'doc', id: 'performance-analysis/basic-actions/gray-areas-on-charts' },
-            { type: 'doc', id: 'performance-analysis/basic-actions/event-tracking' },
-          ],
-        },
+      
+      ],
+    },
+     {
+      collapsed: true,
+      type: 'category',
+      label: 'Real User Monitoring (RUM)',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        { type: 'doc', id: 'digital-sobriety/carbon-footprint-evaluation-and-digital-sobriety' },
+       
       ],
     },
     {
@@ -146,25 +77,89 @@ module.exports = {
     {
       collapsed: true,
       type: 'category',
-      label: 'How-to articles',
+      label: 'Business data',
       link: {
         type: 'generated-index',
       },
       items: [
         { type: 'doc', id: 'how-to-articles/unable-to-connect' },
-        { type: 'doc', id: 'how-to-articles/meaning-of-colors-in-graphs' },
-        { type: 'doc', id: 'how-to-articles/seo-module' },
-        { type: 'doc', id: 'how-to-articles/measurement-interval' },
-        { type: 'doc', id: 'how-to-articles/dem-probes-in-google-analytics' },
-        { type: 'doc', id: 'how-to-articles/faq' },
-        { type: 'doc', id: 'how-to-articles/password-reset' },
-        { type: 'doc', id: 'how-to-articles/using-charts' },
-        { type: 'doc', id: 'how-to-articles/user-journey-screen' },
-        { type: 'doc', id: 'how-to-articles/rum-blocked-by-csp' },
+       
+      ],
+    },
+     {
+      collapsed: true,
+      type: 'category',
+      label: 'System data',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        { type: 'doc', id: 'digital-sobriety/carbon-footprint-evaluation-and-digital-sobriety' },
+      
+      ],
+    },
+     {
+      collapsed: true,
+      type: 'category',
+      label: 'Network data',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        { type: 'doc', id: 'digital-sobriety/carbon-footprint-evaluation-and-digital-sobriety' },
+       
+      ],
+    },
+     {
+      collapsed: true,
+      type: 'category',
+      label: 'Load testing',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        { type: 'doc', id: 'digital-sobriety/carbon-footprint-evaluation-and-digital-sobriety' },
+      
+      ],
+    },
+     {
+      collapsed: true,
+      type: 'category',
+      label: 'Dashboards and exports',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        { type: 'doc', id: 'digital-sobriety/carbon-footprint-evaluation-and-digital-sobriety' },
+      
+      ],
+    },
+    {
+      collapsed: true,
+      type: 'category',
+      label: 'Troubleshooting',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        { type: 'doc', id: 'digital-sobriety/carbon-footprint-evaluation-and-digital-sobriety' },
+       
       ],
     },
     {
       type: 'doc', id: 'release-notes'
-    }
+    },
+     {
+      collapsed: true,
+      type: 'category',
+      label: 'Resources',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        { type: 'doc', id: 'digital-sobriety/carbon-footprint-evaluation-and-digital-sobriety' },
+       
+      ],
+    },
   ],
 };
